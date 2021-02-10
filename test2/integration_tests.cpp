f8eb116773df21e9b6bc9c5c5d4c9a323d84dd7f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-02-10 23:39:59 +0000

    Almost full branch coverage

--- test2/integration_tests.cpp
+++ test2/integration_tests.cpp
@@ -11,3 +11,26 @@ TEST_CASE("empty input") {
   run(input, output);
   Approvals::verify(output.str());
 }
+
+TEST_CASE("real input") {
+  std::string typed = R"(Clare
+0 1
+0 2
+0 3
+0 4
+9 1
+9 2
+9 3
+9 4
+12 1
+12 2
+12 3
+12 4
+
+)";
+  std::stringstream input;
+  input << typed;
+  std::stringstream output;
+  run(input, output);
+  Approvals::verify(output.str());
+}
